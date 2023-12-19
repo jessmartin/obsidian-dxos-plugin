d613bb36699e00312a9d359fec92621b6a5c34c5
Jess Martin <jessmartin@gmail.com>
2023-12-19 11:16:11 -0500

    Fixing indents

--- main.ts
+++ main.ts
@@ -1,152 +1,155 @@
 import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
+  App,
+  Editor,
+  MarkdownView,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
 } from "obsidian";
 
+// import { Client } from "@dxos/client";
+
 interface DXOSSyncPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: DXOSSyncPluginSettings = {
-	mySetting: "default",
+  mySetting: "default",
 };
 
 export default class DXOSSyncPlugin extends Plugin {
-	settings: DXOSSyncPluginSettings;
+  settings: DXOSSyncPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("dxos-sync-plugin-ribbon-class");
+    // const client = new Client();
+    // await client.initialize();
+    // console.log(client.version);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
+    // This creates an icon in the left ribbon.
+    const ribbonIconEl = this.addRibbonIcon(
+      "dice",
+      "Sample Plugin",
+      (evt: MouseEvent) => {
+        // Called when the user clicks the icon.
+        new Notice("This is a notice!");
+      }
+    );
+    // Perform additional things with the ribbon
+    ribbonIconEl.addClass("dxos-sync-plugin-ribbon-class");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    // const statusBarItemEl = this.addStatusBarItem();
+    // statusBarItemEl.setText('Status Bar Text');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: "open-sample-modal-simple",
+      name: "Open sample modal (simple)",
+      callback: () => {
+        new SampleModal(this.app).open();
+      },
+    });
+    // This adds an editor command that can perform some operation on the current editor instance
+    this.addCommand({
+      id: "sample-editor-command",
+      name: "Sample editor command",
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        console.log(editor.getSelection());
+        editor.replaceSelection("Sample Editor Command");
+      },
+    });
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    this.addCommand({
+      id: "open-sample-modal-complex",
+      name: "Open sample modal (complex)",
+      checkCallback: (checking: boolean) => {
+        // Conditions to check
+        const markdownView =
+          this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (markdownView) {
+          // If checking is true, we're simply "checking" if the command can be run.
+          // If checking is false, then we want to actually perform the operation.
+          if (!checking) {
+            new SampleModal(this.app).open();
+          }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+          // This command will only show up in Command Palette when the check function returns true
+          return true;
+        }
+      },
+    });
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
-	}
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+      console.log("click", evt);
+    });
 
-	onunload() {}
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    this.registerInterval(
+      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+    );
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+  onunload() {}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  constructor(app: App) {
+    super(app);
+  }
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.setText("Woah!");
+  }
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: DXOSSyncPlugin;
+  plugin: DXOSSyncPlugin;
 
-	constructor(app: App, plugin: DXOSSyncPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: DXOSSyncPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const { containerEl } = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		new Setting(containerEl)
-			.setName("Setting #3")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
-	}
+    new Setting(containerEl)
+      .setName("Setting #3")
+      .setDesc("It's a secret")
+      .addText((text) =>
+        text
+          .setPlaceholder("Enter your secret")
+          .setValue(this.plugin.settings.mySetting)
+          .onChange(async (value) => {
+            this.plugin.settings.mySetting = value;
+            await this.plugin.saveSettings();
+          })
+      );
+    // .addText((text) => text.setValue(this.plugin.client.version));
+  }
 }
